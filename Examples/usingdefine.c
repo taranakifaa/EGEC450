c0d8531df647b414414961e11a44d578ea1e06a1
Taranaki Faamatuainu <123584947+taranakifaa@users.noreply.github.com>
2026-08-24 15:17:29 -0700

    Add character array definition using STRLEN
    Define STRLEN and create a character array with that length.

---
+++ Examples/usingdefine.c
@@ -0,0 +1,8 @@
+1: #define STRLEN 100
+2: 
+3: int main() {
+4:   char[STRLEN] string;
+5:   // This creates a string (character array) with
+6:   //   space for STRLEN characters.
+7:   // Change STRLEN's value to shrink or lengthen
+8:   //   this array.
