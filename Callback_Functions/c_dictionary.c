efbf7315ee7ac523591d7195304836040a6c4e36
Taranaki Faamatuainu <123584947+taranakifaa@users.noreply.github.com>
2026-09-02 13:37:25 -0700

    Document address calculation and pointer access
    Add documentation for calculating addresses and accessing memory.

---
+++ Callback_Functions/c_dictionary.c
@@ -0,0 +1,10 @@
+3.1. Calculate address from base and index:
+Byte: array_name[index]
+Halfword: array_name[index]
+Word: array_name[index]
+SizeN: array_name[index]
+3.2. Access sequentially using pointers:
+ pt = pt+1; *pt //Byte
+ pt = pt+1; *pt //Halfword
+ pt = pt+1; *pt //Word
+ pt = pt+1; *pt //SizeN:
